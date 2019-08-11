2d051fa8530489fe7843edba6f0d9d0c9f4c4a96
Vignesh S <45061877+telescopic@users.noreply.github.com>
2019-08-11 16:19:02 +0530

    Added Z algorithm

--- template.cpp
+++ template.cpp
@@ -58,3 +58,39 @@ ll fast_exp(ll base, ll expz) {
 	    }
 	    return res%MOD;
 }
+########################################################
+Z Algorithm: 
+// NOTE: Declare string s , ll array z and L,R=0
+int L = 0, R = 0;
+for (int i = 1; i < n; i++) 
+{
+    if (i > R) 
+    {
+        L = R = i;
+        while (R < n && s[R-L] == s[R]) 
+        {
+            R++;
+        }
+        z[i] = R-L; 
+        R--;
+    } 
+    else 
+    {
+        int k = i-L;
+        if (z[k] < R-i+1) 
+        {
+            z[i] = z[k];
+        } 
+        else 
+        {
+            L = i;
+            while (R < n && s[R-L] == s[R]) 
+            {
+                R++;
+            }
+            z[i] = R-L; 
+            R--;
+        }
+    }
+}
+#########################################################
